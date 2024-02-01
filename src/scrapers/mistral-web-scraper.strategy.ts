7c673137ed5bf53cfc6017ffdf2a721b493a0670
Marek Majdak <22301547+marekm1844@users.noreply.github.com>
2024-02-02 00:00:00 +0100

    added mistra, perplexity scrapers

---
+++ src/scrapers/mistral-web-scraper.strategy.ts
@@ -0,0 +1,62 @@
+import { Logger } from '@nestjs/common';
+import { ArticleContentService } from '../scraper/article-content.service';
+import { NewsWithArticle } from '../scraper/news.type';
+import { IScraper } from './scraper.interface';
+import * as cheerio from 'cheerio';
+import { parse } from 'date-fns';
+
+export class MistralWebScraperService implements IScraper {
+  private articleContentService: ArticleContentService;
+
+  constructor() {
+    this.articleContentService = new ArticleContentService();
+  }
+  canHandle(url: string): boolean {
+    //https://mistral.ai/news/
+    return /^https?:\/\/.*mistral.ai.*\//i.test(url);
+  }
+  async scrapeArticle(url): Promise<NewsWithArticle[]> {
+    if (!this.canHandle(url)) {
+      throw new Error('Invalid URL. Only cohare URLs are allowed.');
+    }
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error fetching article: ${response.statusText}`);
+    }
+
+    const html = await response.text();
+    const $ = cheerio.load(html);
+
+    const newsItems = [];
+
+    $('article.border.news-card').each((_, element) => {
+      const title = $(element).find('h4 a').text().trim();
+      const link =
+        'https://mistral.ai/news' + $(element).find('h4 a').attr('href'); // Adjust base URL as necessary
+      const date = $(element).find('li.list-inline-item.pr-4').text().trim();
+      const source = 'Mistral  Website';
+      const company = 'mistral';
+
+      Logger.debug(
+        `[${this.constructor.name}] scrapeArticle: ${title} ${link} Date: ${date} ${source} ${company} `,
+      );
+      newsItems.push({ title, link, date, source, company });
+    });
+
+    const withArticles: NewsWithArticle[] = await Promise.all(
+      newsItems.map(async (item) => {
+        const articleHtml =
+          await this.articleContentService.fetchArticleContent(item.link);
+        const parsedDate = parse(item.date, 'LLL dd, yyyy', new Date());
+        return {
+          ...item,
+          date: parsedDate,
+          innerText: articleHtml,
+        };
+      }),
+    );
+
+    return withArticles;
+  }
+}
---
+++ src/scrapers/perplexity-web-scraper.strategy.ts
@@ -0,0 +1,85 @@
+import { Logger } from '@nestjs/common';
+import { ArticleContentService } from '../scraper/article-content.service';
+import { NewsWithArticle } from '../scraper/news.type';
+import { IScraper } from './scraper.interface';
+import * as cheerio from 'cheerio';
+import { parse } from 'date-fns';
+
+export class MistralWebScraperService implements IScraper {
+  private articleContentService: ArticleContentService;
+
+  constructor() {
+    this.articleContentService = new ArticleContentService();
+  }
+  canHandle(url: string): boolean {
+    //https://blog.perplexity.ai/
+    return /^https?:\/\/.*perplexity.ai.*\//i.test(url);
+  }
+  async scrapeArticle(url): Promise<NewsWithArticle[]> {
+    if (!this.canHandle(url)) {
+      throw new Error('Invalid URL. Only cohare URLs are allowed.');
+    }
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error fetching article: ${response.statusText}`);
+    }
+
+    const html = await response.text();
+    const $ = cheerio.load(html);
+
+    const newsItems = [];
+
+    $('.framer-7tc4vr-container').each((_, container) => {
+      $(container)
+        .find('.framer-sGx6I')
+        .each((_, element) => {
+          const title = $(element).find('h4.framer-text').text().trim();
+          const link =
+            'https://blog.perplexity.ai/blog' +
+            $(element).find('a').attr('href').replace('./blog', '');
+          //const image = $(element).find('img').attr('src');
+          //const content = $(element).find('p.framer-text').text().trim();
+          const date = $(element).find('.framer-1ce0u31 p').text().trim();
+          const source = 'Mistral  Website';
+          const company = 'mistral';
+
+          Logger.debug(
+            `[${this.constructor.name}] scrapeArticle: ${title} ${link} Date: ${date} ${source} ${company} `,
+          );
+          newsItems.push({ title, link, date, source, company });
+        });
+    });
+
+    function isUnique(
+      value: NewsWithArticle,
+      index: number,
+      array: NewsWithArticle[],
+    ) {
+      return (
+        array.findIndex(
+          (item) => item.title === value.title && item.link === value.link,
+        ) === index
+      );
+    }
+
+    const distinctValues = newsItems.filter(isUnique);
+
+    console.log(distinctValues);
+
+    const withArticles: NewsWithArticle[] = await Promise.all(
+      distinctValues.map(async (item) => {
+        const articleHtml =
+          await this.articleContentService.fetchArticleContent(item.link);
+        const parsedDate = parse(item.date, 'MMM dd, yyyy', new Date());
+        return {
+          ...item,
+          date: parsedDate,
+          innerText: articleHtml,
+        };
+      }),
+    );
+
+    return withArticles;
+  }
+}
